a95aab272c67eb5b8cf31202520595b0240e1a58
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-04-15 16:42:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('calculate-btn').addEventListener('click', function() {
     var differenceInTime = endDate.getTime() - startDate.getTime();
     var differenceInDays = differenceInTime / (1000 * 3600 * 24);
 
-    document.getElementById('result').textContent = 'Falta ' + differenceInDays + ' dias para o seu aniversário.';
+    document.getElementById('result').textContent = 'O período entre as datas é de ' + differenceInDays + ' dias.';
   } else {
     document.getElementById('result').textContent = 'Por favor, insira datas válidas.';
   }
