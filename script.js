e624780f54ca674160c95d5f2969cad834e4c007
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-06-17 11:07:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ document.getElementById('calculate-btn').addEventListener('click', function() {
   var endDate = new Date(document.getElementById('end-date').value);
 
   if (startDate && endDate && startDate <= endDate) {
-    var differenceInTime = endDate.getTime() - startDate.getTime();
+    // Convertendo as datas para UTC para evitar problemas com fusos horários
+    var startUTC = Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
+    var endUTC = Date.UTC(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
+
+    var differenceInTime = endUTC - startUTC;
     var differenceInDays = differenceInTime / (1000 * 3600 * 24);
 
     document.getElementById('result').textContent = 'O período entre as datas é de ' + differenceInDays + ' dias.';
